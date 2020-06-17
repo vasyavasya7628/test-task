b25a18dd3afef28bb3e4a47e6aa84bd0db27c57f
Vasily7628 <mineok44@gmail.com>
2020-06-18 02:58:06 +0700

    попытался сделать логическую проверку на наличие повторяющихся эллементов

--- test-app/src/app/app.component.ts
+++ test-app/src/app/app.component.ts
@@ -42,15 +42,85 @@ export class AppComponent {
 
     ]
 
+    logicalSUM(arr: Boolean[]): Boolean {
+
+        var result: Boolean;
+
+        result = false;
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i] || result) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+        return result;
+    }
+
     addItem(id: number, firstName: string, lastName: string): void {
         //var a = this.test.push(new Worker(id, firstName, lastName));
-        if (id == null || firstName.trim() == "" || lastName.trim() == "") return;
+        if (id == null || firstName.trim() == "" || lastName.trim() == "") { return; }
+
+
+
+
+    }
+
+    /*var chek: Boolean;
+    var chekArr: Boolean[] = [];
+
+    chek = false;
+
+    for (var i = 0; i < this.test.length; i++) {
+        if (this.test[i].id != id) {
+            chekArr.push(new Boolean(true));
+        }
+        else {
+            chekArr.push(new Boolean(false));
+        }
+    }
+
+    if (!this.logicalSUM(chekArr)) {
+        this.test.push(new Worker(id, firstName, lastName));
+        return;
+    }
+    */
+    //arr.push(new Worker(id, firstName, lastName));
+    /*
+    overlapCheker(value: Worker, array: Worker[]) {
+        if (value.id != id) {
+            return true;
+        }
+        return false;
+    }
+    */
+
+
+}
+
+/*
         for (let item of this.test) {
             if (this.test.some(item => item.id == id)) {
                 return;
             } else { }
 
         }
-        this.test.push(new Worker(id, firstName, lastName));
+*/
+
+/*
+this.test.forEach(function (value: Worker, index: Number, arr: Worker[]) {
+
+    chek = false;
+    if (value.id == id) {
+        chek = false;
+    } else if (value.id !== id) {
+        chek = true;
     }
-}
+
+    if (chek) {
+        result = true;
+    }
+})
+*/
