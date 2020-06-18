41fedccfb7ac5ec83235932361e96b40c29c8396
Vasily7628 <mineok44@gmail.com>
2020-06-18 14:54:17 +0700

    пытаюсь привести код к стандартам и отказаться от bootstrap в пользу flexbox

--- test-app/src/app/app.component.ts
+++ test-app/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { element } from 'protractor';
 import { worker } from 'cluster';
+import { TestBed } from '@angular/core/testing';
 
 
 class Worker {
@@ -42,63 +43,62 @@ export class AppComponent {
 
     ]
 
-    logicalSUM(arr: Boolean[]): Boolean {
-
-        var result: Boolean;
-
-        result = false;
-        for (var i = 0; i < arr.length; i++) {
-            if (arr[i] || result) {
-                return true;
-            }
-            else {
-                return false;
-            }
-        }
-
-        return result;
-    }
-
     addItem(id: number, firstName: string, lastName: string): void {
+
         //var a = this.test.push(new Worker(id, firstName, lastName));
         if (id == null || firstName.trim() == "" || lastName.trim() == "") { return; }
 
-
-
-
-    }
-
-    /*var chek: Boolean;
-    var chekArr: Boolean[] = [];
-
-    chek = false;
-
-    for (var i = 0; i < this.test.length; i++) {
-        if (this.test[i].id != id) {
-            chekArr.push(new Boolean(true));
-        }
-        else {
-            chekArr.push(new Boolean(false));
+        var findItem = this.test.find((value) => value.id === id)
+        if (findItem !== undefined) {
+            this.test.push(new Worker(id, firstName, lastName));
         }
     }
-
-    if (!this.logicalSUM(chekArr)) {
-        this.test.push(new Worker(id, firstName, lastName));
-        return;
-    }
-    */
-    //arr.push(new Worker(id, firstName, lastName));
-    /*
-    overlapCheker(value: Worker, array: Worker[]) {
-        if (value.id != id) {
-            return true;
-        }
-        return false;
-    }
-    */
-
-
 }
+
+/*
+this.test.find(function (value: Worker, index: Number, arr: Worker[]) {
+        if (value.id == id) {
+
+            value.id = arr.push(new Worker(id, firstName, lastName));
+        } else { arr.push(new Worker(id, firstName, lastName)); }
+    });
+*/
+/*var chek: Boolean;
+var chekArr: Boolean[] = [];
+
+chek = false;
+.............
+     if (this.id == id) {
+        Worker[this.id] = new Worker(id, firstName, lastName);
+
+    }
+.................
+for (var i = 0; i < this.test.length; i++) {
+    if (this.test[i].id != id) {
+        chekArr.push(new Boolean(true));
+    }
+    else {
+        chekArr.push(new Boolean(false));
+    }
+}
+
+if (!this.logicalSUM(chekArr)) {
+    this.test.push(new Worker(id, firstName, lastName));
+    return;
+}
+*/
+    //arr.push(new Worker(id, firstName, lastName));
+/*
+overlapCheker(value: Worker, array: Worker[]) {
+    if (value.id != id) {
+        return true;
+    }
+    return false;
+}
+*/
+
+
+
 
 /*
         for (let item of this.test) {
